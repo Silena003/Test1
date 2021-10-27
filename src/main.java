eb339c43676dcd27983f099eca5ebab87de73bc4
Elena <silena003@gmail.com>
2021-10-27 12:07:09 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int accountBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int total = accountBalance + transferAmount;
+        System.out.println(total);
+    }
+}
